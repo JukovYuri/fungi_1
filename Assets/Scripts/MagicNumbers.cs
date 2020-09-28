51da3b0228ba4cc60021232a15b6cbec935a73ae
JukovYuri <jukov_yuri@mail.ru>
2020-09-29 01:58:42 +0300

    111

---
+++ Assets/Scripts/MagicNumbers.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class MagicNumbers : MonoBehaviour
+{
+	int moves;
+	public Text textMoves;
+
+	public int min = 1;
+	public int max = 1000;
+	public Text textMinMax;
+
+	int guess;
+	public Text textGuess;
+
+	void Start()
+	{
+		min = 1;
+		max = 1000;
+		moves = 0;
+		textMinMax.text = $"Загадайте число \nот <color=#ff0000ff>{min}</color> до <color=#ff0000ff>{max}</color>...";
+		UpdateGuess();
+	}
+
+	void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.UpArrow))
+		{
+			min = guess;
+			UpdateGuess();
+		}
+
+		if (Input.GetKeyDown(KeyCode.DownArrow))
+		{
+			max = guess;
+			UpdateGuess();
+		}
+
+		if (Input.GetKeyDown(KeyCode.Return) || Input.GetKeyDown(KeyCode.KeypadEnter))
+		{
+			textMoves.text = $"Количество ходов: <color=#ff0000ff>{moves}</color>";
+		}
+
+	}
+
+	void UpdateGuess()
+		{
+		guess = (min + max) / 2;
+		moves++;
+		textGuess.text = guess.ToString();
+	}
+}
